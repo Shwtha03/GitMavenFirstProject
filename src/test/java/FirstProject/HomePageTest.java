3b6043936ddf0555543d3429212d51a31c1f9684
swetha <shwtha03@gmail.com>
2022-03-24 22:08:25 +0800

    commited login page of Rediff

--- src/test/java/FirstProject/HomePageTest.java
+++ src/test/java/FirstProject/HomePageTest.java
@@ -14,7 +14,19 @@ public class HomePageTest extends base{
 	public void browserNavigation() throws IOException 
 	{
 		driver =initializeDriver();
+		//driver.get(url);
 		
+		RediffHomePage rd = new RediffHomePage(driver);
+		RediffLoginPage lp = new RediffLoginPage(driver);
+		
+		
+		rd.Signin().click();
+		lp.username().sendKeys("swetha");
+		lp.password().sendKeys("rao@gmail.com");
+		lp.signin().click();
+		
+		
+		//driver.findElements(By.linkText("Sign in"));
 		//driver.findElement(By.id("flight-tab")).click();
 		
 		//;
